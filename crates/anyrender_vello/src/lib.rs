1f400d94172ba1dd10d97501cee823912f7fb6ab
Nico Burns <nico@nicoburns.com>
2026-05-10 22:06:24 +0100

    Make anyrender_vello work on wasm
    Signed-off-by: Nico Burns <nico@nicoburns.com>

--- crates/anyrender_vello/src/lib.rs
+++ crates/anyrender_vello/src/lib.rs
@@ -1,8 +1,11 @@
 //! A [`vello`] backend for the [`anyrender`] 2D drawing abstraction
+
+#[cfg(not(target_arch = "wasm32"))]
 mod image_renderer;
 mod scene;
 mod window_renderer;
 
+#[cfg(not(target_arch = "wasm32"))]
 pub use image_renderer::VelloImageRenderer;
 pub use scene::VelloScenePainter;
 pub use wgpu_context::DeviceHandle;
